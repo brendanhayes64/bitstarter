9e6e9418ee090f5f3b47adf0fca47cd40f725ffa
Brendan Hayes <brendanhayes64@gmail.com>
2013-07-10 23:51:36 +0000

    closed app.get function

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
     var fs = require('fs');
     var buf = new Buffer(fs.readFileSync('index.html'));
     response.send(buf.toString());
-}
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
